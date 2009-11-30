c92a474ab9dc101cca13c020706e38561ce101fa
koehlersb747 <koehlersb747@2103374a-2dac-11de-958b-d3ca58845339>
2009-11-30 23:06:26 +0000

    Added i/o utility BufferedStringReader

---
+++ modules/io/src/java/org/sd/io/BufferedStringReader.java
@@ -0,0 +1,216 @@
+/*
+    Copyright 2009 Semantic Discovery, Inc. (www.semanticdiscovery.com)
+
+    This file is part of the Semantic Discovery Toolkit.
+
+    The Semantic Discovery Toolkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Semantic Discovery Toolkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with The Semantic Discovery Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.sd.io;
+
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Utility to read strings from a reader.
+ * <p>
+ * @author Spence Koehler
+ */
+public class BufferedStringReader {
+
+  static final int IO_BUFFER_SIZE = 1024;
+
+  private final char[] ioBuffer;
+  private final StringBuilder stringBuilder;
+  private int ioBufferOffset;
+  private int ioBufferEnd;
+  private int charOffset;
+
+  public BufferedStringReader() {
+    this(IO_BUFFER_SIZE);
+  }
+
+  public BufferedStringReader(int ioBufferSize) {
+    this.ioBuffer = new char[ioBufferSize];
+    this.stringBuilder = new StringBuilder();
+
+    init();
+  }
+
+  /**
+   * Get the offset of the next character to be read.
+   */
+  public int getCharOffset() {
+    return charOffset;
+  }
+
+  /**
+   * Read all data from the reader as a string.
+   */
+  public String readFully(Reader reader) throws IOException {
+
+    grabChars();  // grab any pending characters
+
+    while (loadChars(reader) > 0) {
+      if (!grabChars()) {
+        break;
+      }
+    }
+
+    final String result = stringBuilder.toString();
+    stringBuilder.setLength(0);
+    return result;
+  }
+
+  /**
+   * Read all characters until (and not including) the given character is
+   * encountered or the end of the reader is reached.
+   */
+  public String readUntil(Reader reader, char c) throws IOException {
+    if (!grabUntil(c)) {
+      while (loadChars(reader) > 0) {
+        if (grabUntil(c)) {
+          break;
+        }
+      }
+    }
+
+    final String result = stringBuilder.toString();
+    stringBuilder.setLength(0);
+    return result;
+  }
+
+  /**
+   * Read while the characters encountered are 'c'.
+   *
+   * @return the read characters, possibly empty.
+   */
+  public String readWhile(Reader reader, char c) throws IOException {
+    if (grabWhile(c)) {
+      while (loadChars(reader) > 0) {
+        if (!grabWhile(c)) {
+          break;
+        }
+      }
+    }
+
+    final String result = stringBuilder.toString();
+    stringBuilder.setLength(0);
+    return result;
+  }
+
+  /**
+   * Reset this instance.
+   */
+  public void reset() {
+    stringBuilder.setLength(0);
+    init();
+  }
+
+  private final void init() {
+    ioBufferOffset = 0;
+    ioBufferEnd = 0;
+    charOffset = 0;
+  }
+
+  /**
+   * Load characters into the buffer.
+   */
+  private final int loadChars(Reader reader) throws IOException {
+    final int numRead = reader.read(ioBuffer, ioBufferEnd, ioBuffer.length - ioBufferEnd);
+
+    if (numRead >= 0) {
+      ioBufferEnd += numRead;
+    }
+
+    return numRead;
+  }
+
+  /**
+   * Grab all chars currently in the io buffer.
+   */
+  private final boolean grabChars() {
+    boolean result = false;
+
+    if (ioBufferEnd > ioBufferOffset) {
+      final int diff = ioBufferEnd - ioBufferOffset;
+
+      // capture any pending characters
+      stringBuilder.append(ioBuffer, ioBufferOffset, diff);
+
+      // reset the io buffer
+      charOffset += diff;
+      ioBufferOffset = 0;
+      ioBufferEnd = 0;
+
+      result = true;
+    }
+
+    return result;
+  }
+
+  /**
+   * Grab all chars currently in the io buffer until the character is reached.
+   *
+   * @return true if the character was reached.
+   */
+  private final boolean grabUntil(char c) {
+    boolean result = false;
+
+    for (; ioBufferOffset < ioBufferEnd; ++ioBufferOffset) {
+      final char curc = ioBuffer[ioBufferOffset];
+      if (curc == c) {
+        result = true;
+        break;
+      }
+      stringBuilder.append(curc);
+      ++charOffset;
+    }
+
+    // reset the io buffer
+    if (ioBufferOffset == ioBufferEnd) {
+      ioBufferOffset = 0;
+      ioBufferEnd = 0;
+    }
+
+    return result;
+  }
+
+  /**
+   * Grab all chars currently in the io buffer while the character occurs.
+   *
+   * @return true if the character may still occur.
+   */
+  private final boolean grabWhile(char c) {
+    boolean result = true;
+
+    for (; ioBufferOffset < ioBufferEnd; ++ioBufferOffset) {
+      final char curc = ioBuffer[ioBufferOffset];
+      if (curc != c) {
+        result = false;
+        break;
+      }
+      stringBuilder.append(curc);
+      ++charOffset;
+    }
+
+    // reset the io buffer
+    if (ioBufferOffset == ioBufferEnd) {
+      ioBufferOffset = 0;
+      ioBufferEnd = 0;
+    }
+
+    return result;
+  }
+}
---
+++ modules/io/src/test/junit/org/sd/io/TestBufferedStringReader.java
@@ -0,0 +1,167 @@
+/*
+    Copyright 2009 Semantic Discovery, Inc. (www.semanticdiscovery.com)
+
+    This file is part of the Semantic Discovery Toolkit.
+
+    The Semantic Discovery Toolkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Semantic Discovery Toolkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with The Semantic Discovery Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.sd.io;
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * JUnit Tests for the BufferedStringReader class.
+ * <p>
+ * @author Spence Koehler
+ */
+public class TestBufferedStringReader extends TestCase {
+
+  public TestBufferedStringReader(String name) {
+    super(name);
+  }
+  
+
+  private final String buildLargeString(int size, int lowChar, int highChar) {
+    final StringBuilder stringBuilder = new StringBuilder();
+
+    final int nchars = highChar - lowChar + 1;
+    for (int i = 0; i < size; ++i) {
+      stringBuilder.append((char)(lowChar + (i % nchars)));
+    }
+
+    return stringBuilder.toString();
+  }
+
+  public void testReadFully() throws IOException {
+    // initialize a string reader using chars from ascii 33(!) to 126(~)
+    final String string = buildLargeString((int)(BufferedStringReader.IO_BUFFER_SIZE * 2.5), '!', '~');
+    final StringReader reader = new StringReader(string);
+
+    // initialize a buffered string reader
+    final BufferedStringReader bsr = new BufferedStringReader();
+    assertEquals(0, bsr.getCharOffset());
+
+    // read fully
+    final String got = bsr.readFully(reader);
+
+    // check
+    assertEquals(string, got);
+    assertEquals(string.length(), bsr.getCharOffset());
+  }
+
+  public void testReadUntilWhileAndFully() throws IOException {
+    // initialize a string reader
+    final String largeString = buildLargeString((int)(BufferedStringReader.IO_BUFFER_SIZE * 2.5), 'a', 'z');
+    final String[] strings = new String[] {
+      largeString,
+      "   ",
+      "foobarbaz",
+      "       ",
+      largeString,
+    };
+    
+    final StringBuilder concat = new StringBuilder();
+    for (String string : strings) concat.append(string);
+    final StringReader reader = new StringReader(concat.toString());
+
+    // initialize a buffered string reader
+    final BufferedStringReader bsr = new BufferedStringReader();
+    assertEquals(0, bsr.getCharOffset());
+    int len = 0;
+
+    // read until ' '
+    final String gotUntil1 = bsr.readUntil(reader, ' ');
+    len += gotUntil1.length();
+    assertEquals(strings[0], gotUntil1);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read while ' '
+    final String gotWhile1 = bsr.readWhile(reader, ' ');
+    len += gotWhile1.length();
+    assertEquals(strings[1], gotWhile1);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read until ' '
+    final String gotUntil2 = bsr.readUntil(reader, ' ');
+    len += gotUntil2.length();
+    assertEquals(strings[2], gotUntil2);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read while ' '
+    final String gotWhile2 = bsr.readWhile(reader, ' ');
+    len += gotWhile2.length();
+    assertEquals(strings[3], gotWhile2);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read fully (remainder)
+    final String remainder = bsr.readFully(reader);
+    len += remainder.length();
+    assertEquals(strings[4], remainder);
+    assertEquals(len, bsr.getCharOffset());
+  }
+
+  public void testReadWhileAcrossBufferBoundary() throws IOException {
+    // initialize a string reader
+    final String largeString = buildLargeString((int)(BufferedStringReader.IO_BUFFER_SIZE - 2), 'a', 'z');
+    final String[] strings = new String[] {
+      largeString,
+      "     ",
+      "foobarbaz",
+    };
+    
+    final StringBuilder concat = new StringBuilder();
+    for (String string : strings) concat.append(string);
+    final StringReader reader = new StringReader(concat.toString());
+
+    // initialize a buffered string reader
+    final BufferedStringReader bsr = new BufferedStringReader();
+    assertEquals(0, bsr.getCharOffset());
+    int len = 0;
+
+    // read until ' '
+    final String gotUntil1 = bsr.readUntil(reader, ' ');
+    len += gotUntil1.length();
+    assertEquals(strings[0], gotUntil1);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read while ' '
+    final String gotWhile1 = bsr.readWhile(reader, ' ');
+    len += gotWhile1.length();
+    assertEquals(strings[1], gotWhile1);
+    assertEquals(len, bsr.getCharOffset());
+
+    // read fully (remainder)
+    final String remainder = bsr.readFully(reader);
+    len += remainder.length();
+    assertEquals(strings[2], remainder);
+    assertEquals(len, bsr.getCharOffset());
+  }
+
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(TestBufferedStringReader.class);
+    return suite;
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+}
