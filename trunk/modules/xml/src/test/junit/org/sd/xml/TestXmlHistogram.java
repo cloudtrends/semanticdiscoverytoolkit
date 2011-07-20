6e9d006ce1da95c8bfe5a67d78b550e9c683752e
koehlersb747 <koehlersb747@2103374a-2dac-11de-958b-d3ca58845339>
2011-07-20 16:08:51 +0000

    Adding xml utility layer for histograms

---
+++ trunk/modules/xml/src/test/junit/org/sd/xml/TestXmlHistogram.java
@@ -0,0 +1,67 @@
+/*
+    Copyright 2011 Semantic Discovery, Inc. (www.semanticdiscovery.com)
+
+    This file is part of the Semantic Discovery Toolkit.
+
+    The Semantic Discovery Toolkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The Semantic Discovery Toolkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with The Semantic Discovery Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.sd.xml;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * JUnit Tests for the XmlHistogram class.
+ * <p>
+ * @author Spence Koehler
+ */
+public class TestXmlHistogram extends TestCase {
+
+  public TestXmlHistogram(String name) {
+    super(name);
+  }
+  
+
+  public void testRoundTrip() {
+    // construct a histogram
+    final XmlHistogram xh = new XmlHistogram();
+    xh.add("test", 3);
+    xh.add("it", 5);
+
+    // convert to xml
+    final XmlStringBuilder xml = xh.asXml("h");
+
+    // reconstitute from xml
+    final XmlHistogram xh2 = new XmlHistogram(xml);
+
+    // verify content
+    assertEquals(2, xh2.getNumRanks());
+    assertEquals("it", xh2.getElement(0));
+    assertEquals(5, xh2.getFrequencyCount(0));
+    assertEquals("test", xh2.getElement(1));
+    assertEquals(3, xh2.getFrequencyCount(1));
+  }
+
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(TestXmlHistogram.class);
+    return suite;
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+}
