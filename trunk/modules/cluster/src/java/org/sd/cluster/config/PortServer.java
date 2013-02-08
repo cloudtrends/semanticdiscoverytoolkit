e8936ca5b20f462a75a9f44130ea34f478f1a8a6
koehlersb747 <koehlersb747@2103374a-2dac-11de-958b-d3ca58845339>
2013-02-08 22:31:15 +0000

    Moving test harness code to source area for reference from consumers

--- trunk/modules/cluster/src/java/org/sd/cluster/config/PortServer.java
+++ trunk/modules/cluster/src/java/org/sd/cluster/config/PortServer.java
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with The Semantic Discovery Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 */
-package org.sd.testtools;
+package org.sd.cluster.config;
 
 
 import org.sd.io.FileLock;
